314da6ed6893309701637f6ee6f0ad7899ae50a9
Artem Shevtsov (home) <artemka12@gmail.com>
2016-02-04 22:00:51 +0200

    Added userDAO and user entity classes

--- src/main/java/learn/spring/dao/UserDAO.java
+++ src/main/java/learn/spring/dao/UserDAO.java
@@ -1,7 +1,23 @@
 package learn.spring.dao;
 
-/**
- * Created by aftor on 04.02.16.
- */
+import learn.spring.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
 public class UserDAO {
+    static public Map<Integer, User> usersMap;
+
+    public User getUserById(int id){
+        return null;
+    }
+
+    public User getUserByEmail(String email){
+        return null;
+    }
+
+    public User getUserByName(String name){
+        return null;
+    }
 }
--- src/main/java/learn/spring/services/UserService.java
+++ src/main/java/learn/spring/services/UserService.java
@@ -1,7 +1,25 @@
 package learn.spring.services;
 
+import learn.spring.dao.UserDAO;
+import learn.spring.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    public User getUserById(int id){
+        return userDAO.getUserById(id);
+    }
+
+    public User getUserByEmail(String email){
+        return userDAO.getUserByEmail(email);
+    }
+
+    public User getUserByName(String name){
+        return userDAO.getUserByName(name);
+    }
 }
